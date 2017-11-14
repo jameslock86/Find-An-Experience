5531d633f47282118eab1484cdaffcb8f6e8d57b
jameslock86 <31215180+jameslock86@users.noreply.github.com>
2017-11-14 08:54:20 -0600

    monday work

--- README.md
+++ README.md
@@ -34,6 +34,3 @@ Have you ever went on a vacation or visited a new city for the weekend and wante
 7. HTML
 8. CSS
 9. Node
-
-
-#Wire Frame
--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+	//trail api key
+	const key = 'dx08NystFzmshq8Ij2Xe7pH8n4xfp1WguRQjsncf8vQ1BdN8mu';
+	//google maps key
+	const key2 = 'AIzaSyAQse9dG32PSHCCebegk2KeBTfmuvLH6fc';
+	const map = [];
 
-	let key = 'dx08NystFzmshq8Ij2Xe7pH8n4xfp1WguRQjsncf8vQ1BdN8mu';
 
 
-	function doThings(response) {
+	function appendMap(response) {
+		for (let i in response.places) {
+			// console.log(response.places);
+			let lat = response.places[i].lat;
+			let lon = response.places[i].lon;
+			let multipleMarkers = response.places[i].activities;
+			console.log(multipleMarkers);
+			let mapInput = `<iframe
+		  width="300"
+		  height="175"
+		  frameborder="0"
+		  scrolling="no"
+		  marginheight="0"
+		  marginwidth="0"
+		  src="https://maps.google.com/maps?q=${lat},${lon}&hl=es;z=14&amp;output=embed">
+		 </iframe>
+		 <br />
+		 <small>
+		   <a
+		    href="https://maps.google.com/maps?q='+data.lat+','+data.lon+'&hl=es;z=14&amp;output=embed"
+		    style="color:#0000FF;text-align:left"
+		    target="_blank"
+		   >
+		     See map bigger
+		   </a>
+		 </small>`;
+
+			if (lat !== 0 && lon !== 0) {
+				$('.map').append(mapInput);
+			}
+
+
+		}
+	}
+
+
+
+
+
+
+	function printOutMyCityStateArr(response) {
+		//first pass gets you city amd state
+
 		for (let i in response) {
 			$('.responses').append(response[i]);
-			console.log('here in the loop', i, response[i]);
+			//second pass gets you park name
+
+
+			for (let q in response[i]) {
+				console.log('the park name is', (response[i][q]).name);
+				let name;
+				for (let w in response[i][q]) {
+					let activities;
+					console.log('things to do in parks', (response[i][q]).activities);
+				}
+
+			}
+
+
+
 		}
 
 	}
-
-	function goTo() {
-		location.href = document.getElementById(city, state).value;
-		console.log(city, state);
-	}
-
-
-
-
-
-
-
-
-	function getCityState(city,state) {
+	//this is where we make the call to the api
+	function getCityState(city, state) {
 		let getSite = `https://trailapi-trailapi.p.mashape.com/?q[city_cont]=${city}&q[state_cont]=${state}`;
 		$.ajax({
 			url: getSite,
@@ -35,23 +83,27 @@ $( document ).ready(function() {
 			datatype: 'JSON',
 		})
 			.then(function(response) {
-				console.log('RESPONSE FIRST:', response);
-				doThings(response);
+				appendMap(response);
 			})
 			.catch(function(error) {
 				console.log('error: ', error);
 			});
 	}
-
+	//this is where the click gets you the city to populte
 	$('#findCity').click(function(e) {
 		e.preventDefault();
 		let city;
 		let state;
 		city = $('#find-city').val();
 		state = $('#find-state').val();
-		getCityState(city,state);
-
+		getCityState(city, state);
 	});
 
+	// $('#findCity').click(renderOnMap() {
+	// 	// make a marker when called
+	//
+	// }) {
+	//
+	// }
 
 });
