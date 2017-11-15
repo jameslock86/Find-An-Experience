adb048367740409df80e71040ee749db34402c81
jameslock86 <31215180+jameslock86@users.noreply.github.com>
2017-11-15 13:39:05 -0600

    140 pm i have commented out stuff i dont need in my js file and working on making windows pop um in the small maps

--- googleMap.js
+++ googleMap.js
@@ -1,9 +1,31 @@
-function initMap() {
-	var uluru= {};
-	var map= new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
-		center: uluru
-	});
-
-
-}
+// function initMap() {
+// 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+// 		center: { lat: 34.397, lng: 150.644 },
+// 		scrollwheel: false,
+// 		zoom: 2
+// 	});
+//
+// 	setPath(function(data) {
+// 		addPath(map,data);
+// 	});
+// }
+//
+// function setPath(callback) {
+// 	$.getJSON('https://gist.githubusercontent.com/vgrem/91ba4d694157169b112c/raw/5bdd81c6f5bdfa5ba2d0ca8d5494129b329399d8/expOneActivityData.json',
+// 		function (data) {
+// 			callback(data);
+// 		}
+// 	);
+// }
+//
+//
+// function addPath(map,expeditionCoordinates) {
+// 	var trekLine = new google.maps.Polyline({
+// 		path: expeditionCoordinates,
+// 		geodisc: true,
+// 		stokeColor: '#FF0000',
+// 		strokeOpacity: 1.0,
+// 		strokeWeight: 2
+// 	});
+// 	trekLine.setMap(map);
+// }
