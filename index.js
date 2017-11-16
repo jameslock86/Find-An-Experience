7b8f67c394f803471e7aadc09ebf6bd08a7d329b
jameslock86 <31215180+jameslock86@users.noreply.github.com>
2017-11-16 14:23:22 -0600

    table fixed minus the activities

--- index.js
+++ index.js
@@ -48,25 +48,28 @@ $(document).ready(function() {
 	// 	}
 	// }
 
-
-	// <tbody id="tbody"></tbody>
 	function appendMap(response) {
+		console.log('before', response);
+
+		parks.activities[0];
+		parks.activities[1];
+		let some = response.places;
+		var $parks = $('#parks');
+		console.log(some);
+		$.each(some, function(i, parks) {
+			$parks.append('<br><li>city: ' + parks.city + '<li> state:' + parks.state + '<li>name: ' + parks.name + '<li>activity: ' +   '</li><br>');
+			// thingsToDo +
+		});
 		for (let i in response.places) {
-			//console.log(response.places);
-
-			let some = response.places;
-			var $parks= $('#parks');
-
-			$.each(some,function (i, parks) {
-				$parks.append('<br><li>city: '+ parks.city +'<li> state:'+ parks.state +'<li>name: '+ parks.name +'<li>activity: '+ parks.name.multipleMarkers + '</li><br>');
-
-			});
-
 			var lat = response.places[i].lat;
 			var lon = response.places[i].lon;
-			let multipleMarkers = response.places[i].activities;
+			let multipleMarkers = response.places[0].activities;
+			for (let q = 0; q < multipleMarkers.length; q++) {
+				console.log('heres this', multipleMarkers[q].activity_type_name);
+				let thingsToDo = multipleMarkers[q].activity_type_name;
 
-			console.log(multipleMarkers);
+			}
+
 			width = '800';
 			height = '400';
 			frameborder = '0';
@@ -103,11 +106,11 @@ $(document).ready(function() {
 			$('.responses').append(response[i]);
 			//second pass gets you park name
 			for (let q in response[i]) {
-				//console.log('the park name is', (response[i][q]).name);
+				// console.log('the park name is', (response[i][q]).name);
 				let name;
 				for (let w in response[i][q]) {
 					let activities;
-					console.log('things to do in parks', (response[i][q]).activities);
+					//console.log('things to do in parks', (response[i][q]).activities);
 				}
 			}
 		}
@@ -125,8 +128,7 @@ $(document).ready(function() {
 			datatype: 'JSON',
 		})
 			.then(function(response) {
-				// googlemapdots = response;
-				// console.log(googlemapdots);
+
 				appendMap(response);
 			})
 			.catch(function(error) {
@@ -143,84 +145,3 @@ $(document).ready(function() {
 		getCityState(city, state);
 	});
 });
-
-
-
-
-
-
-
-
-//
-// jQuery(function($) {
-// 	// Asynchronously Load the map API
-// 	var script = document.createElement('script');
-// 	script.src = '//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize';
-// 	document.body.appendChild(script);
-// });
-//
-// function initialize() {
-// 	var map;
-// 	var bounds = new google.maps.LatLngBounds();
-// 	var mapOptions = {
-// 		mapTypeId: 'roadmap'
-// 	};
-//
-// 	// Display a map on the page
-// 	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-// 	map.setTilt(45);
-//
-// 	// Multiple Markers
-// 	var markers = [
-// 		['London Eye, London', 51.503454, -0.119562],
-// 		['Palace of Westminster, London', 51.499633, -0.124755]
-// 	];
-//
-// 	// Info Window Content
-// 	var infoWindowContent = [
-// 		['<div class="info_content">' +
-//       '<h3>London Eye</h3>' +
-//       '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' + '</div>'
-// 		],
-// 		['<div class="info_content">' +
-//       '<h3>Palace of Westminster</h3>' +
-//       '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
-//       '</div>'
-// 		]
-// 	];
-//
-// 	// Display multiple markers on a map
-// 	var infoWindow = new google.maps.InfoWindow(),
-// 		marker, i;
-//
-// 	// Loop through our array of markers & place each one on the map
-// 	for (i = 0; i < markers.length; i++) {
-// 		var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-// 		bounds.extend(position);
-// 		marker = new google.maps.Marker({
-// 			position: position,
-// 			map: map,
-// 			title: markers[i][0]
-// 		});
-//
-// 		// Allow each marker to have an info window
-// 		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-// 			return function() {
-// 				infoWindow.setContent(infoWindowContent[i][0]);
-// 				infoWindow.open(map, marker);
-// 			};
-// 		})(marker, i));
-//
-// 		// Automatically center the map fitting all markers on the screen
-// 		map.fitBounds(bounds);
-// 	}
-//
-// 	// Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-// 	var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-// 		this.setZoom(14);
-// 		google.maps.event.removeListener(boundsListener);
-// 	});
-//
-//
-//
-// }
