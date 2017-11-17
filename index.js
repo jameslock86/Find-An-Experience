4d819c7e4dab057a2aa286ea434ee74f4dbaa467
jameslock86 <31215180+jameslock86@users.noreply.github.com>
2017-11-17 08:54:46 -0600

    done

--- index.js
+++ index.js
@@ -2,52 +2,15 @@ let googlemapdots;
 
 let map;
 
-// window.initMap = function() {
-// 	var map = new google.maps.Map(
-// 		document.getElementById('map_canvas'), {
-// 			center: new google.maps.LatLng(`${lat,lon}`),
-// 			zoom: 10,
-// 			mapTypeId: google.maps.MapTypeId.ROADMAP
-// 		});
-// 	google.maps.event.addDomListener(window, 'load', initialize());
-
-
-
-
-
 $(document).ready(function() {
 	//trail api key
 	const key = 'dx08NystFzmshq8Ij2Xe7pH8n4xfp1WguRQjsncf8vQ1BdN8mu';
 	//google maps key
 	const key2 = 'AIzaSyAQse9dG32PSHCCebegk2KeBTfmuvLH6fc';
 
-	// function initialize() {
-	//   var map = new google.maps.Map(
-	//     document.getElementById('map_canvas'), {
-	//       center: new google.maps.LatLng(`${lat,lon}`),
-	//       zoom: 10,
-	//       mapTypeId: google.maps.MapTypeId.ROADMAP
-	// 	  });
-	// 	google.maps.event.addDomListener(window, 'load', initialize());
 
-	//
-	// // Create a <script> tag and set the USGS URL as the source.
-	// var script = document.createElement('script');
-	// script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAQse9dG32PSHCCebegk2KeBTfmuvLH6fc&libraries=places';
-	// document.getElementById('map_wrapper').appendChild(script);
-
-	// Loop through the results array and place a marker for each
-	// set of coordinates.
-	// function bigMap(data) {
-	// 	for (var i = 0; i < data.length; i++) {
-	// 		var latLng = new google.maps.LatLng(data[i].lat, data[i].lon);
-	// 		var marker = new google.maps.Marker({
-	// 			position: latLng,
-	// 			map: map
-	// 		});
-	// 	}
-	// }
-
+	// this is where i am putting the info into the maps
+	// also this is where i am putting the info into the tables as well
 	function appendMap(response) {
 		console.log('before', response);
 
@@ -81,19 +44,10 @@ $(document).ready(function() {
 		  src="https://maps.google.com/maps?q=${lat},${lon}&hl=es;z=14&amp;output=embed">
 		 </iframe>
 		 <br />`;
-			// <med>
-			//  <a
-			//   href="https://maps.google.com/maps?q='+data.lat+','+data.lon+'&hl=es;z=14&amp;output=embed"
-			//    style="color:#0000FF;"
-			//   target="_blank">
-			//  </a>
-			//  </med>`;
+
 
 			if (lat !== 0 && lon !== 0) {
 				$('.map').append(mapInput, multipleMarkers);
-
-
-
 
 			}
 		}
